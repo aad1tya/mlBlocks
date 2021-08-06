58790de59bc9e323f41d44892dc4e65d52709e8a
Aditya Kumawat <adityakumawat502@gmail.com>
2021-08-06 15:26:42 +0530

    Added an initializer and some docstrings

--- new_file.py
+++ new_file.py
@@ -1,20 +1,31 @@
 import math
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x = 0, y = 0):
+        """Initializes the position of a new point. The x and y
+           coordinates can be specified. If not specified, they
+           will get defaulted to 0"""
         self.move(x, y)
 
     def move(self, x, y):
+        """Move a point in the 2D space using x and y values"""
         self.x = x
         self.y = y
 
     def reset(self):
+        """Reset the points to zero aka the origin"""
         self.move(0, 0)
     
     def calculate_distance(self, other_point):
+        """Calculates the distance between two points using the
+           Pythagorean Theorem. The return value is a float.
+           
+           Parameters:
+           other_point = The passed value should be an object instantiated
+                         from a class"""
         return math.sqrt(
-            (self.x - other_point.x)**2 +
-                (self.y - other_point.y)**2
+            (self.x - other_point.x)**2
+            +    (self.y - other_point.y)**2
         )
 
 point1 = Point(4, 3)
