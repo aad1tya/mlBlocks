cd6935df83c5b619575876b3acef2e546d0d5017
Aditya Kumawat <adityakumawat502@gmail.com>
2021-08-07 15:37:04 +0530

    Removed a few examples

--- base/LinearRegression.py
+++ base/LinearRegression.py
@@ -5,34 +5,25 @@ class LinearRegression():
     def __init__(self, steps, learning_rate):
         self.steps = steps
         self.learning_rate = learning_rate
-        self.m = len(self.label)
-        self.theta = np.zeros((self.m, 1)) 
-        self.h = np.dot(self.train_set.T, self.theta)
-    
-    def fit_on_data(self, train_set, label):
-        #Preprocessing data before doing anything else.
-        self.train_set = train_set
-        self.label = label
-        train_set, label = self.data_preprocess()
-
-        self.theta = self.gradient_descent()
-        print("Done!")
     
     def data_preprocess(self):
-        return (np.array(self.train_set), np.array(self.label)) 
+        return (np.array(self.train_set), np.array(self.label))
     
-    def gradient_descent(self, h):
-        self.h = h
+    def gradient_descent(self):
         for i in range(1, self.steps):
             second_half = (1/self.m) * (np.sum(np.dot((self.h - self.label), self.train_set)))
-            self.theta = self.theta - self.learning_rate * second_half 
+            theta = theta - self.learning_rate * second_half
         
-        return self.theta
+        return theta
+
 
     def get_cost(self):
+        m = len(self.label)
+        theta = np.zeros((m, 1))
+        h = np.dot(self.train_set.T, theta)
 
-        cost_p1 = (1/2*self.m)
-        cost_p2 = np.sum(np.square(self.h - self.label))
+        cost_p1 = (1/2*m)
+        cost_p2 = np.sum(np.square(h - self.label))
         cost = cost_p1 * cost_p2
         return cost
     
@@ -42,11 +33,3 @@ class LinearRegression():
 
 
 
-    
-X = [4, 5, 6, 3, 2, 2]
-y = [2, 3, 4, 2, 1, 1]
-
-ob1 = LinearRegression(20, 0.01)
-
-ob1.fit_on_data(train_set=X, label=y)
-print(ob1.get_predictions([5, 3]))
