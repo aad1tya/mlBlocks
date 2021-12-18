4c3f1a3d05b465cd379af7d8368efca376bf564e
Aditya Kumawat <adityakumawat502@gmail.com>
2021-12-18 22:22:04 +0530

    Added docstring for metrics/r_squared file

--- metrics/r_squared.py
+++ metrics/r_squared.py
@@ -2,7 +2,15 @@ from residual_sum_of_squares import residual_sum_of_squares
 from total_sum_of_squares import total_sum_of_squares
 
 def r_squared(true_values, predicted_values):
+    '''true_values: Your target's values (must be an iterable)
+       predicted_values: Values predicted by your model (must be an iterable)
+
+       This function returns (1 - rss/tss)
+       
+       r_squared is an alternative measure of fit of your model to the data. The value
+       is always in between 0 and 1.'''
+
     rss = residual_sum_of_squares(true_values=true_values, predicted_values=predicted_values)
     tss = total_sum_of_squares(true_values)
     return (1 - rss/tss)
-    
+
