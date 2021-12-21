092742316ccf2d7e90a5bbb8f7800e985449adcc
Aditya Kumawat <adityakumawat502@gmail.com>
2021-12-21 21:47:07 +0530

    Added multiple linear regression

--- base/SimpleLinearRegression.py
+++ base/SimpleLinearRegression.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+
 class SimpleLinearRegression():
 
     def sample_mean(self, sample):
@@ -13,8 +15,8 @@ class SimpleLinearRegression():
 
     def coefficient_calculation(self, predictors, response):
         n = len(response)
-        x = predictors
-        y = response
+        x = predictors.values.tolist()
+        y = response.values.tolist()
         xt = [[x[j][i] for j in range(len(x))] for i in range(len(x[0]))]
 
         first_term = np.linalg.inv(np.dot(xt, x))
@@ -33,3 +35,20 @@ class SimpleLinearRegression():
         except:
             raise Exception("Unexpected Error Occurred")
 
+def residual_sum_of_squares1(true_values, predicted_values):
+    length = len(true_values)
+    sum_of_squared_values = 0
+    
+    for i in range(0, length):
+        difference = true_values[i] - predicted_values[i]
+        squared_value = difference**2
+        sum_of_squared_values += squared_value
+
+    return float(sum_of_squared_values)
+
+ob1 = SimpleLinearRegression()
+data = pd.read_csv("C:/Users/ak111/OneDrive/Documents/50_Startups.csv")
+x = data[data.columns[:-2]]
+y = data['Profit']
+print(residual_sum_of_squares1(ob1.fit(x, y), y.values.tolist()))
+print(y)
